137717e08c8c931044c3712c6d416767ec8aafd4
zarafulcher <zarafulcher@gmail.com>
2026-09-14 18:45:25 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'ledger-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
